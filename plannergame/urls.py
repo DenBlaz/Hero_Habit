10ead819bdca27b1530f0195d5b3ef7b0c72bbf9
n0rn <norn35590@gmail.com>
2024-12-16 17:46:32 +0200

    Update Dashboard

--- plannergame/urls.py
+++ plannergame/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('accounts/', include('accounts.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
