3f5cfb5e6a280983f3f6cd720c4bf1b032d1c076
n0rn <norn35590@gmail.com>
2024-12-16 16:30:34 +0200

    Update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from .views import UserLoginView
 
 urlpatterns = [
     path('sign/', views.sign_up, name = "sign"),
-    path('login/', views.login_view, name = "login"),
+    path('login/', UserLoginView.as_view(), name = "login"),
     path('profile/', views.profile, name = "profile"),
 ]
