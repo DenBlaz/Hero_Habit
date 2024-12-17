87eaf5dffccc75038a70452337a3f5ada7fa3885
n0rn <norn35590@gmail.com>
2024-12-17 15:31:17 +0200

    Update html

--- plannergame/settings.py
+++ plannergame/settings.py
@@ -133,14 +133,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
+    BASE_DIR / "static",
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
