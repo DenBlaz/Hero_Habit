7a0407177865ec4f37ce1fbbd26b29b3e08a23a7
n0rn <norn35590@gmail.com>
2024-12-20 03:33:23 +0200

    Update html

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,12 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.dashboardviews, name = "dashboard"),
+
+    path('mon', views.monviews, name = "mon"),
+    path('tue', views.tueviews, name = "tue"),
+    path('wed', views.wedviews, name = "wed"),
+    path('thu', views.thuviews, name = "thu"),
+    path('fri', views.friviews, name = "fri"),
+    path('sat', views.satviews, name = "sat"),
+    path('sun', views.sunviews, name = "sun"),
 ]
