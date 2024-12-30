ddcda096a3950eb8991942f57901c79d4c09b92f
n0rn <norn35590@gmail.com>
2024-12-30 15:26:23 +0200

    Upd

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,12 +5,5 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.dashboardviews, name = "dashboard"),
-
-    path('mon', views.monviews, name = "mon"),
-    path('tue', views.tueviews, name = "tue"),
-    path('wed', views.wedviews, name = "wed"),
-    path('thu', views.thuviews, name = "thu"),
-    path('fri', views.friviews, name = "fri"),
-    path('sat', views.satviews, name = "sat"),
-    path('sun', views.sunviews, name = "sun"),
+    path('/<str:day>/', views.dashboardviews, name='dashday'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,18 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def dashboardviews(request):
     return render(request, "dashboard/dashboard.html")
-
-def monviews(request):
-    return render(request, "dashboard/dashboard.html")
-def tueviews(request):
-    return render(request, "dashboard/dashboard.html")
-def wedviews(request):
-    return render(request, "dashboard/dashboard.html")
-def thuviews(request):
-    return render(request, "dashboard/dashboard.html")
-def friviews(request):
-    return render(request, "dashboard/dashboard.html")
-def satviews(request):
-    return render(request, "dashboard/dashboard.html")
-def sunviews(request):
-    return render(request, "dashboard/dashboard.html")
