82419f004f403776cd6d1680eed5e5d98ad19543
Davyd-S <davyd.sereda.pm.2023@lpnu.ua>
2025-02-11 14:28:12 +0200

    Update script.js
    Added a new button which is going to redirect to tasks for month page, and deleted a blank which was at that button's place

--- front-end/dashboard/script.js
+++ front-end/dashboard/script.js
@@ -8,28 +8,27 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
 
         if (showingWeekdays) {
-
             daysSection.innerHTML = `
-        <button class="active-day rounded">Fri</button>
-        <button class="day rounded">Sat</button>
-        <button class="day rounded">Sun</button>
-        <div class="blank"></div>
-        <a class="other" href="#" id="arrow-btn">
-          <img src="images/icons/arrow2.svg" alt="arrow" />
-        </a>
-      `;
+    <button class="active-day rounded">Fri</button>
+    <button class="day rounded">Sat</button>
+    <button class="day rounded">Sun</button>
+    <button class="a-month rounded">Tasks for month</button><!-- should redirect to tasks for month page-->
+    <!-- deleted "blank div" -->
+    <button class="other" id="arrow-btn">
+      <img src="arrow2.svg" />
+    </button>
+  `;
         } else {
-
             daysSection.innerHTML = `
-        <button class="active-day rounded">Mon</button>
-        <button class="day rounded">Tue</button>
-        <button class="day rounded">Wed</button>
-        <button class="day rounded">Thu</button>
-        <a class="other" href="#" id="arrow-btn">
-          <img src="images/icons/arrow1.svg" alt="arrow" "/>
-        </a>
-      `;}
-
+    <button class="active-day rounded">Mon</button>
+    <button class="day rounded">Tue</button>
+    <button class="day rounded">Wed</button>
+    <button class="day rounded">Thu</button>
+    <button class="other" id="arrow-btn">
+      <img src="arrow1.svg"  "/>
+    </button>
+  `;
+        }
 
         assignArrowClick();
         showingWeekdays = !showingWeekdays;
@@ -43,9 +42,23 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
-// JavaScript to toggle content display based on list item clicks
 
-//Davyd's code:
+const modal = document.getElementById("profile-modal");
+const openModalBtn = document.getElementById("open-modal");
+
+openModalBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+    openModalBtn.classList.add("circled");
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+        openModalBtn.classList.remove("circled");
+    }
+});
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const toggleIcon = document.getElementById("toggle-icon");
     const closeIcon = document.getElementById("close");
