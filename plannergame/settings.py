86d0e9c43603baba0ddac719cafeb6b42ea40d07
n0rn552 <norn35590@gmail.com>
2025-02-14 12:32:44 +0200

    Upd DATABASE in settings.py

--- plannergame/settings.py
+++ plannergame/settings.py
@@ -81,16 +81,18 @@ WSGI_APPLICATION = 'plannergame.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'testdb',
-#        'USER': 'myuser',
-#        'PASSWORD': 'mypassword',
-#        'HOST': 'localhost',
-#        'PORT': '5432',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
+3
+
 
 DATABASES = {
     'default': dj_database_url.config(
