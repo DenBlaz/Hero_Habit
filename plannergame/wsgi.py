4676b9277a48e9a174a26d2125b87ed90d14af95
n0rn552 <norn35590@gmail.com>
2025-02-14 12:38:09 +0200

    Upd wsgi

--- plannergame/wsgi.py
+++ plannergame/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plannergame.settings')
 
 application = get_wsgi_application()
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    execute_from_command_line("manage.py", "runserver", "0.0.0.0:" + os.environ.get("PORT", "8000"))
+
