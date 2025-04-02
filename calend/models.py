9d5a7fa6f9e8822e02fbff239318c074b233e6c2
n0rn552 <norn35590@gmail.com>
2025-04-02 18:34:00 +0300

    upd dailytask

--- calend/models.py
+++ calend/models.py
@@ -5,17 +5,18 @@ from accounts.models import Account
 class DailyTask(models.Model):
 
     user = models.ForeignKey(Account, on_delete=models.CASCADE) 
+    
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-
-    entered_date = models.DateField(null=True, blank=True)
-    start_time = models.TimeField(null=True, blank=True)
-    finish_time = models.TimeField(null=True, blank=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
+    characteristic = models.CharField(max_length=255, blank=True, null=True)
+    
+    start_time = models.DateTimeField(blank=True, null=True)
+    finish_time = models.DateTimeField(blank=True, null=True)
+    
+    due_date = models.DateTimeField(blank=True, null=True)
     completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
 
     def __str__(self):
         return f"{self.title} ({self.user.email})"
