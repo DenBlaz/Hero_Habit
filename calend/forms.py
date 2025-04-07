a21f81ff3b6112b8eb965e12ffa6bf590f7a9059
n0rn552 <norn35590@gmail.com>
2025-04-07 19:39:46 +0300

    upd forms

--- calend/forms.py
+++ calend/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import DailyTask
+from .models import DailyTask, LongTask
 
 class DailyTaskForm(forms.ModelForm):
     due_date = forms.DateField(input_formats=['%d.%m.%Y'])
@@ -7,8 +7,11 @@ class DailyTaskForm(forms.ModelForm):
     class Meta:
         model = DailyTask
         fields = ['title', 'description', 'characteristic', 'start_time', 'finish_time', 'due_date']
-        widgets = {
-            'due_date': forms.DateInput(attrs={'placeholder': 'DD.MM.YYYY'}),
-            'start_time': forms.TimeInput(attrs={'placeholder': '00:00'}),
-            'finish_time': forms.TimeInput(attrs={'placeholder': '23:59'}),
-        }
+
+class LongTaskForm(forms.ModelForm):
+    start_date = forms.DateField(input_formats=['%d.%m.%Y'])
+    finish_date = forms.DateField(input_formats=['%d.%m.%Y'])
+
+    class Meta:
+        model = LongTask
+        fields = ['title', 'description', 'characteristic', 'start_date', 'finish_date']
