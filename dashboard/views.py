a60151e0a930d7aa908874559469491604513c72
Den_Blazh <denisb53502006@gmail.com>
2025-04-08 05:52:47 +0300

    upd from denys achievements

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
+from achievements.models import UserAchievement
+
 
 def dashboardviews(request):
     user = request.user
+    achievements = UserAchievement.objects.filter(user=user).select_related('achievement')
     if user.main_characteristic is None:
         return redirect('accounts:character_create')
-    return render(request, "dashboard/dashboard.html")
     
+    last_achievement = achievements.last() if achievements.exists() else None
+
+    return render(request, "dashboard/dashboard.html", {
+        'last_achievement': last_achievement
+    })
